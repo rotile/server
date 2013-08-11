ab827353518e17b2604d470f394dff401ecfaab3
bastiendavid <bastien.david.info@gmail.com>
2013-08-11 12:11:43 +0200

    Test refactoring
    Refactored the test of the ProjectService class.

--- src/test/java/com/open/rotile/service/ProjectServiceTest.java
+++ src/test/java/com/open/rotile/service/ProjectServiceTest.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
@@ -15,20 +16,27 @@ import com.open.rotile.service.persist.ProjectPersistService;
 
 public class ProjectServiceTest {
 
+	private ProjectPersistService projectPersistService;
+	private ProjectService service;
+	final String projectName = "my project";
+	final int vote = 3;
+
+	@Before
+	public void setUp() {
+		projectPersistService = Mockito.mock(ProjectPersistService.class);
+		service = new ProjectService(projectPersistService);
+	}
+
 	@Test
 	public void createProject_create_new_project()
 			throws ProjectAlreadyExistException {
 		// Given
-		ProjectPersistService projectPersistService = Mockito
-				.mock(ProjectPersistService.class);
-		ProjectService service = new ProjectService(projectPersistService);
-		final String projectName = "my project";
-
-		// When
 		ArgumentCaptor<Project> argCaptor = ArgumentCaptor
 				.forClass(Project.class);
 		Mockito.when(projectPersistService.createProject(argCaptor.capture()))
 				.thenReturn(true);
+
+		// When
 		service.createProject(projectName);
 
 		// Then
@@ -41,11 +49,6 @@ public class ProjectServiceTest {
 	public void createProject_throws_exception_if_project_already_exist()
 			throws ProjectAlreadyExistException {
 		// Given
-		ProjectPersistService projectPersistService = Mockito
-				.mock(ProjectPersistService.class);
-		ProjectService service = new ProjectService(projectPersistService);
-		final String projectName = "my project";
-
 		Mockito.when(
 				projectPersistService.createProject(Mockito.any(Project.class)))
 				.thenReturn(false);
@@ -60,12 +63,6 @@ public class ProjectServiceTest {
 	@Test
 	public void vote_vote_for_project() throws ProjectDoesNotExistException {
 		// Given
-		ProjectPersistService projectPersistService = Mockito
-				.mock(ProjectPersistService.class);
-		ProjectService service = new ProjectService(projectPersistService);
-		final int vote = 3;
-		final String projectName = "my project";
-
 		Mockito.when(projectPersistService.voteForProject(projectName, vote))
 				.thenReturn(true);
 
@@ -80,12 +77,6 @@ public class ProjectServiceTest {
 	public void vote_throws_exception_if_project_does_not_exist()
 			throws ProjectDoesNotExistException {
 		// Given
-		ProjectPersistService projectPersistService = Mockito
-				.mock(ProjectPersistService.class);
-		ProjectService service = new ProjectService(projectPersistService);
-		final int vote = 3;
-		final String projectName = "my project";
-
 		Mockito.when(projectPersistService.voteForProject(projectName, vote))
 				.thenReturn(false);
 
@@ -99,10 +90,6 @@ public class ProjectServiceTest {
 	@Test
 	public void findProject_return_project_if_exists() {
 		// Given
-		ProjectPersistService projectPersistService = Mockito
-				.mock(ProjectPersistService.class);
-		ProjectService service = new ProjectService(projectPersistService);
-		final String projectName = "my project";
 		Project expectedProject = new Project(projectName);
 		Mockito.when(projectPersistService.findProject(projectName))
 				.thenReturn(expectedProject);
@@ -117,9 +104,6 @@ public class ProjectServiceTest {
 	@Test
 	public void listProjects_return_all_projects_like() {
 		// Given
-		ProjectPersistService projectPersistService = Mockito
-				.mock(ProjectPersistService.class);
-		ProjectService service = new ProjectService(projectPersistService);
 		List<Project> allProjects = new ArrayList<Project>();
 		allProjects.add(new Project("my project"));
 		allProjects.add(new Project("my project 2"));
