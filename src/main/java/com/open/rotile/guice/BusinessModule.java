ac2868cfe9ad32328afa07d7b611b9dfe8727c21
bastiendavid <bastien.david.info@gmail.com>
2013-08-12 14:57:35 +0200

    Removed VoteService and updated index.html
    VoteService has been removed. A vote can only be attached to a project.
    The index.html page has been updated, to replace the projects.html,
    which allows projects browsing and creation.

--- src/main/java/com/open/rotile/guice/BusinessModule.java
+++ src/main/java/com/open/rotile/guice/BusinessModule.java
@@ -2,13 +2,9 @@ package com.open.rotile.guice;
 
 import com.google.inject.AbstractModule;
 import com.open.rotile.service.IProjectService;
-import com.open.rotile.service.IVoteService;
 import com.open.rotile.service.ProjectService;
-import com.open.rotile.service.VoteService;
 import com.open.rotile.service.persist.IProjectPersistService;
-import com.open.rotile.service.persist.IVotePersistService;
 import com.open.rotile.service.persist.ProjectPersistService;
-import com.open.rotile.service.persist.VotePersistService;
 
 public class BusinessModule extends AbstractModule {
 
@@ -16,8 +12,6 @@ public class BusinessModule extends AbstractModule {
 	protected void configure() {
 		// Bind all business services
 		bind(IProjectService.class).to(ProjectService.class);
-		bind(IVoteService.class).to(VoteService.class);
-		bind(IVotePersistService.class).to(VotePersistService.class);
 		bind(IProjectPersistService.class).to(ProjectPersistService.class);
 	}
 }
